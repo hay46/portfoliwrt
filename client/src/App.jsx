77e04b450c076977a477eb3022d003a2be9e744f
haymanot <haymanotebabu2@gmail.com>
2025-08-07 17:35:02 -0700

    first comit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from "./component/header_page/Header";
+import Hero from '../src/component/hero_page/Hero';
+import Signup from '../../../../signup/frontend/src/Signup_page/Signup'
+function App() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Signup />
+    </div>
+  );
+}
+
+export default App
---
+++ client/src/component/hero_page/Hero.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './hero.css'
+import logo from '../../assets/image_folder/portfolio logo.png'
+function Hero() {
+  return (
+    <div className="div-portfolio">
+      <div className="div-all">
+        <div className="div-img">
+          <img src={logo} alt="" />
+        </div>
+
+        <div className="div-name">
+          <h1 className="h1-name">
+            <span>i'm haymanot ebabu</span>,and also <br />
+            fullstack web developer
+          </h1>
+        </div>
+        <div className="div-discribtion">
+          <p>
+            As a passionate and versatile Full-Stack Developer, I specialize in
+            building robust, <br />
+            scalable,scalable, and user-centric web applications from concept to
+            deployment. <br />
+            With hands-on experience across both front-end and back-end <br />
+            technologies, I bring a holistic approach to software <br />
+            development—merging intuitive design with powerful functionality
+          </p>
+        </div>
+        <div className="contact-resume">
+          <h2>contact with me</h2>
+          <h3>my resume</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero
